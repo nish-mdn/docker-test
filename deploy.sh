d2951e739935de5c110fb865e6af9e4bfb88036e
MadhanAyyasamy <madhantry@gmail.com>
2025-10-03 18:38:51 +0530

    stopping existing container comment it out

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#set -e
+set -e
 
 REPO_URI=177701659471.dkr.ecr.us-east-1.amazonaws.com/mdn-docker-repo
 
-echo "Stopping old container..."
-docker stop rails-app || true
-docker rm rails-app || true
+# echo "Stopping old container..."
+# docker stop rails-app || true
+# docker rm rails-app || true
 
 echo "Pulling latest image..."
 aws ecr get-login-password --region us-east-1 \
