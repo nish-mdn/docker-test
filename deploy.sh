f9c740457e76214772f7bb2b9f949ae9ecc6baa7
MadhanAyyasamy <madhantry@gmail.com>
2025-10-03 18:02:18 +0530

    appspec added

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-REPO_URI=177701659471.dkr.ecr.<region>.amazonaws.com/myapp
+REPO_URI=177701659471.dkr.ecr.us-east-1.amazonaws.com/mdn-docker-repo
 
 echo "Stopping old container..."
-docker stop myapp || true
-docker rm myapp || true
+docker stop rails-app || true
+docker rm rails-app || true
 
 echo "Pulling latest image..."
 aws ecr get-login-password --region us-east-1 \
@@ -13,4 +13,5 @@ aws ecr get-login-password --region us-east-1 \
 docker pull $REPO_URI:latest
 
 echo "Starting new container..."
-docker run -d --name myapp -p 80:3000 $REPO_URI:latest
+#docker run -d --name myapp -p 80:3000 $REPO_URI:latest
+docker run -it --rm --name rails-app -p 3000:3000 --link db:db -e DB_HOST=db mdn-docker-repo
