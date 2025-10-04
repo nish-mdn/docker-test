0cdf7464e619bb70604b52c505dab52ace906e0c
MadhanAyyasamy <madhantry@gmail.com>
2025-10-04 08:19:59 +0530

    db migration removed

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ export BUNDLE_GEMFILE=/myapp/Gemfile
 # rails -v
 # echo "bundler version"
 # bundle --version
-echo "Running migrations (if needed) and seeding..."
-bundle exec rails db:migrate 2>/dev/null || bundle exec rails db:setup
+#echo "Running migrations (if needed) and seeding..."
+#bundle exec rails db:migrate 2>/dev/null || bundle exec rails db:setup
 
 exec "$@"
