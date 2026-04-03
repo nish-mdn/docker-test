afe884969750f36baad1536492e4cfd296e150fa
madhan.ayyasamy <madhantry@gmail.com>
2026-04-03 14:21:25 +0530

    auth service url configured

--- config/initializers/auth_service.rb
+++ config/initializers/auth_service.rb
@@ -1,1 +1,1 @@
-Rails.application.config.auth_service_url = ENV.fetch("AUTH_SERVICE_URL", "http://localhost:3001")
+Rails.application.config.auth_service_url = ENV.fetch("AUTH_SERVICE_URL", "http://13.222.3.101:30080/")
