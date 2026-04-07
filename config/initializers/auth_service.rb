bc9b665b767f6a6f366ccf11f9ab7ca91b2de8c8
madhan.ayyasamy <madhantry@gmail.com>
2026-04-07 13:03:26 +0530

    auth service public ip changed

--- config/initializers/auth_service.rb
+++ config/initializers/auth_service.rb
@@ -1,1 +1,1 @@
-Rails.application.config.auth_service_url = ENV.fetch("AUTH_SERVICE_URL", "http://13.222.3.101:30080/")
+Rails.application.config.auth_service_url = ENV.fetch("AUTH_SERVICE_URL", "http://54.163.36.240:30080/")
