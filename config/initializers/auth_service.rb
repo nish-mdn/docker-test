76236517010f7f784a36a42a1ba545204b4c683a
madhan.ayyasamy <madhantry@gmail.com>
2026-04-08 21:35:32 +0530

    docker test full flow main app triggers manifest repo and do k8s oidc deployment

--- config/initializers/auth_service.rb
+++ config/initializers/auth_service.rb
@@ -1,1 +1,1 @@
-Rails.application.config.auth_service_url = ENV.fetch("AUTH_SERVICE_URL", "http://54.163.36.240:30080/")
+Rails.application.config.auth_service_url = ENV.fetch("AUTH_SERVICE_URL", "http://34.226.212.144:30080/")
